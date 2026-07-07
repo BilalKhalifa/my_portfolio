204620e8c19d97522fa3f8d88913e94e7394971d
BilalKhalifa <bilalkhalifa9988@gmail.com>
2026-07-07 14:42:39 +0530

    feat: Make Hero Page Responsive

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -34,15 +34,21 @@ const Hero = () => {
   },[])
 
   return (
-    <div className='relative h-full w-full pt-50 flex bg-primary'>
-        <div ref={leftRef} className="left flex flex-col items-start w-1/2 h-full pl-95">
+    <div className='
+                    relative h-full w-full pt-28 md:pt-50 flex flex-col-reverse md:flex-row
+                  bg-primary gap-12 md:gap-0 pb-16 md:pb-0
+                  '
+    >
+      {/* Left Container */}
+        <div ref={leftRef} 
+            className="left flex flex-col items-start w-full md:w-1/2 h-full px-6 md:pl-20 xl:pl-95">
             <div 
             
             className='
                 flex items-center
-                text-white bg-(--transparent-pill)/10 
-                px-4 py-2 rounded-full border border-(--transparent-pill)/10 
-                uppercase font-[Montserrat] font-light text-[11px] tracking-[3px]
+                text-white bg-transparent-pill/10 
+                px-4 py-2 rounded-full border border-transparent-pill/10 
+                uppercase font-nav font-light text-[11px] tracking-[3px]
                 mb-8
             '>
               <div className="relative flex items-center justify-center w-3 h-3 mr-3">
@@ -52,16 +58,39 @@ const Hero = () => {
               </div>
               Available • {new Date().getFullYear()}
             </div>
-            <div className='hero-title will-change-transform  bg-linear-to-r from-[#f4f8f9] to-[#97ddf4] bg-clip-text text-transparent'>Engineering </div>
-            <div className='hero-title will-change-transform bg-linear-to-r from-[#f4f8f9] to-[#97ddf4] bg-clip-text text-transparent'>Digital </div>
-            <div className='hero-title will-change-transform bg-linear-to-r from-[#84e2fa] via-[#9dd7f7] to-[#b2a0fe] bg-clip-text text-transparent italic'>Experiences</div>
-            <div className='mt-7 font-[Montserrat] text-white/50 text-lg font-light '>
+            <div 
+                className='
+                            font-hero text-5xl md:text-[104px] leading-tight md:leading-[110px]
+                            font-semibold  text-transparent
+                            bg-linear-to-r from-[#f4f8f9] to-[#97ddf4] bg-clip-text 
+                          '
+            >
+              Engineering 
+            </div>
+            <div 
+                className='
+                            font-hero text-5xl md:text-[104px] leading-tight md:leading-[110px]
+                            font-semibold  text-transparent
+                            bg-linear-to-r from-[#f4f8f9] to-[#97ddf4] bg-clip-text
+                          '
+            >
+              Digital 
+            </div>
+            <div className='
+                            font-hero text-5xl md:text-[104px] leading-tight md:leading-[110px]
+                            font-semibold  text-transparent italic
+                            bg-linear-to-r from-[#84e2fa] via-[#9dd7f7] to-[#b2a0fe] bg-clip-text
+                            '
+            >
+              Experiences
+            </div>
+            <div className='mt-7 font-nav text-white/50 text-base md:text-lg font-medium md:font-light max-w-xl '>
               <p>I'm <span className='font-bold text-white'>Bilal Khalifa</span> — a Computer Engineer and Full Stack Developer crafting interfaces that feel <span className='text-[#97E6F5]'>inevitable</span>: fast, elegant, considered to the pixel.
               </p>
             </div>
-            <div className='flex gap-5 mt-5 text-sm font-[Montserrat] font-medium items-center '>
+            <div className='flex gap-5 mt-5 text-sm font-nav font-medium items-center '>
                 <button 
-                        className='bg-white flex gap-2 rounded-full  py-3 px-6 items-center cursor-pointer'
+                        className='bg-white flex gap-2 rounded-full  py-3 px-6 items-center cursor-pointer whitespace-nowrap'
                         onMouseEnter={()=>arrowRef.current?.startAnimation()}
                         onMouseLeave={()=>arrowRef.current?.stopAnimation()}
                 >
@@ -69,7 +98,7 @@ const Hero = () => {
                 </button>
                 <button 
                   className='flex gap-3
-                  text-white items-center 
+                  text-white items-center whitespace-nowrap
                     border border-white/10 rounded-full
                   bg-white/5 py-3 px-6 cursor-pointer tracking-widest'
                   onMouseEnter={()=>mailRef.current?.startAnimation()}
@@ -78,13 +107,14 @@ const Hero = () => {
                   Get in touch <MailFilledIcon className='size-7' ref={mailRef}/>
                 </button>
             </div>
-            <div className='font-[Montserrat] text-white/50 mt-12 text-[10px] tracking-[3px] flex items-center gap-4'>
+            <div className='font-nav text-white/50 mt-12 text-[10px] tracking-[3px] flex items-center gap-4'>
               <span className='h-px w-14 bg-white/30 inline-block'></span> 
               SCROLL TO BEGIN 
               <span className='h-px w-14 bg-white/30 inline-block'></span>
             </div>
         </div>
-        <div ref={rightRef} className="right will-change-transform w-1/2 h-full relative pl-50">
+        {/* Right Container */}
+        <div ref={rightRef} className="right will-change-transform w-full md:w-1/2 h-full relative px-6 md:pl-20 xl:pl-50 flex justify-center md:justify-start">
           <motion.div 
             animate={{rotate:360}}
             transition={{
@@ -92,7 +122,7 @@ const Hero = () => {
               repeat: Infinity,
               ease: "linear"
             }}
-            className='absolute  blur-3xl opacity-50 bg-linear-to-t from-[#2f5f96] to-[#823ebd] h-131.75 w-97.5'>
+            className='absolute  blur-3xl opacity-50 bg-linear-to-t from-[#2f5f96] to-[#823ebd] h-96 w-96 md:h-131.75 md:w-97.5'>
           </motion.div>
         <motion.div 
 
@@ -105,32 +135,39 @@ const Hero = () => {
             ease: "easeInOut"
           }}
           className='
-                          will-change-transform antialiased backface-hidden
-                          z-20 absolute border-2 border-white/10 h-135 w-97.5 rounded-4xl
-                          backdrop-blur-4xl bg-transparent-pill/20'
+                      z-20 relative border-2 border-white/10 h-fit md:h-135 w-full max-w-97.5 rounded-4xl md:rounded-4xl
+                      backdrop-blur-4xl bg-transparent-pill/20 p-4 md:p-0 flex flex-col md:block  gap-4
+                      will-change-transform antialiased backface-hidden
+                    '
         >
-            <div className="profile w-85.25 h-108 m-5 
-                            rounded-4xl overflow-hidden
-            ">
-                <img src='/images/MyImage.png'/>
+            <div className=" 
+                            relative profile w-full h-[360px] md:w-85.25 md:h-108 md:m-5 
+                            rounded-3xl md:rounded-4xl overflow-hidden
+                          "
+            >
+                <img 
+                    src='/images/MyImage.png'
+                    alt='Bilal Khalifa'
+                    className='h-full w-full object-cover'
+                />
             </div>
-            <div className="description w-full flex">
-              <div className="left flex flex-col w-1/2 ml-7 -mt-2">
-                  <span className='text-white font-[SpaceGrotesk] text-[20px] font-semibold'>Bilal Khalifa</span>
-                  <span className='font-[Montserrat] text-xs text-[#97E6F5] italic '>Computer Engineer</span>
+            <div className="description w-full flex justify-between items-start px-2 md:px-0 mt-2 md:mt-0">
+              <div className="left flex flex-col  md:ml-7 -mt-2">
+                  <span className='text-white font-hero text-[20px] font-semibold leading-none whitespace-nowrap'>Bilal Khalifa</span>
+                  <span className='font-[Montserrat] text-xs text-[#97E6F5] italic mt-1 whitespace-nowrap '>Computer Engineer</span>
               </div>
               <div 
-                  className="right flex flex-col w-1/2 gap-1 
-                  font-[Montserrat] text-[10px] text-white/50
-                  items-end mr-7 -mt-2"
+                  className="right flex flex-col gap-2 md:gap-1
+                  font-nav text-[10px] text-white/50
+                  items-end md:mr-7 -mt-2"
               >
-                <p className='flex gap-2'>
+                <p className='flex gap-2 items-center whitespace-nowrap'>
                   <RiMapPinLine size={15} /> Eru,Navsari, India
                 </p>
-                <p className='flex gap-2'>
+                <p className='flex gap-2 items-center whitespace-nowrap'>
                   <RiCpuLine size={15} /> MERN STACK
                 </p>
-                <p className='flex gap-2 text-[#7DDDFB]'>
+                <p className='flex gap-2 text-[#7DDDFB] items-center whitespace-nowrap'>
                   <RiBardLine size={15} /> Open To Work
                 </p>
               </div>
